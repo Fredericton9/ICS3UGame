4203caea6b0a4380b98725ef17f86bd379376370
Neobdragon <Neobdragon@hotmail.com>
2015-04-20 21:29:34 -0400

    Added Vertical Grid

--- Chess/WIP/Game.py
+++ Chess/WIP/Game.py
@@ -525,7 +525,33 @@ def is_checkmated():#Checkmate function
             return 0
         deselect_piece()
     return 1
+def GridSystem():
+    grid_horizontal = font.render("a     b     c     d     e     f     g     h", 0, (0,255,0))
+    game_menu.blit(grid_horizontal,(120,500))
 
+    gridvert1 = font.render("1", 1, (0,255,0))
+    game_menu.blit(gridvert1, (80,110))
+
+    gridvert2 = font.render("2", 1, (0,255,0))
+    game_menu.blit(gridvert2, (80,160))
+
+    gridvert3 = font.render("3", 1, (0,255,0))
+    game_menu.blit(gridvert3, (80,210))
+
+    gridvert4 = font.render("4", 1, (0,255,0))
+    game_menu.blit(gridvert4, (80,260))
+
+    gridvert5 = font.render("5", 1, (0,255,0))
+    game_menu.blit(gridvert5, (80,310))
+
+    gridvert6 = font.render("6", 1, (0,255,0))
+    game_menu.blit(gridvert6, (80,360))
+
+    gridvert7 = font.render("7", 1, (0,255,0))
+    game_menu.blit(gridvert7, (80,410))
+
+    gridvert8 = font.render("8", 1, (0,255,0))
+    game_menu.blit(gridvert8, (80,460))
 
 """ Game variables """
 
@@ -776,8 +802,7 @@ while running:
             if game_board[i] == 'N2':
                 game_menu.blit(knight2,(100+(i%8)*50,450-(i//8)*50))
     font = pygame.font.Font(None, 36)
-    text = font.render("a     b     c     d     e     f     g     h", 1, (0, 255, 0))
-    game_menu.blit(text,(120,500))
+    GridSystem()
     screen.fill((0,0,255))
     draw.rect(screen,(255,0,0),(100,100,400,400))
     draw_menus(screen)
